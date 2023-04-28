a70e92c68c2439c138c62aad1902cfeadf695c93
Chiranjeewee Koirala <kchiranjewee63@gmail.com>
2023-04-28 15:49:37 -0400

    Add status messages

--- main.py
+++ main.py
@@ -6,19 +6,26 @@ from modelEvaulation import calAccuracy
 
 spark = SparkSession.builder.appName("SentimentAnalysis").getOrCreate()
 
+print("Loading data...")
 data = IMDBMovieReviewData(spark, "IMDB Dataset.csv")
 data.loadData()
+
+print("Preprocessing data...")
 data.preprocessData()
+
+print("Splitting data into training and testing set...")
 data.splitData()
 
-
+print("Training multinomial naive bayse on IMBD data...")
 parameters = train(data.training_data)
 
 
 
+print("Calculating train accuracy...")
+print(f"Train Accuracy: {calAccuracy(data.training_data, parameters)}")
 
-calAccuracy(data.training_data, parameters)
-calAccuracy(data.testing_data, parameters)
+print("Calculating test accuracy...")
+print(f"Test Accuracy: {calAccuracy(data.testing_data, parameters)}")
 
 
 
