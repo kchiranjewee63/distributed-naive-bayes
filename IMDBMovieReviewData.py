9d176070dca2f8529998fa9ae7f4d53c6f1aa6c8
Chiranjeewee Koirala <kchiranjewee63@gmail.com>
2023-04-29 01:52:19 -0400

    Refactor preprocessing function

--- IMDBMovieReviewData.py
+++ IMDBMovieReviewData.py
@@ -15,22 +15,18 @@ class IMDBMovieReviewData:
         self.movie_reviews_df = self.spark_session.read.csv(self.data_path, header=True, inferSchema=True, quote='"', escape='"')
 
     def preprocessData(self):
-        # Convert words to lowercase
-        self.movie_reviews_df = self.movie_reviews_df.withColumn("review", lower(self.movie_reviews_df.review))
-
-        # Remove special characters
-        self.movie_reviews_df = self.movie_reviews_df.withColumn("review", regexp_replace(self.movie_reviews_df.review, "[^a-zA-Z0-9\\s]", ""))
-
-        # Remove stop words
         stop_words = set(stopwords.words('english'))
-        self.movie_reviews_df = self.movie_reviews_df.withColumn("words", split(self.movie_reviews_df.review, "\\s+")).drop("review")
-        remove_stop_words_udf = udf(lambda review: [word for word in review if word not in stop_words], ArrayType(StringType()))
-        self.movie_reviews_df = self.movie_reviews_df.withColumn("nonStopWords", remove_stop_words_udf(self.movie_reviews_df.words)).drop("words")
-
-        # Stemming
         stemmer = SnowballStemmer(language='english')
-        stem_words_udf = udf(lambda review: " ".join([stemmer.stem(word) for word in review]), StringType())
-        self.movie_reviews_df = self.movie_reviews_df.withColumn("review", stem_words_udf(self.movie_reviews_df.nonStopWords)).drop("nonStopWords")
+
+        remove_stop_words_udf = udf(lambda x: [word for word in x if word not in stop_words], ArrayType(StringType()))
+        stem_udf = udf(lambda x: ' '.join([stemmer.stem(word) for word in x]), StringType())
+
+        self.movie_reviews_df = self.movie_reviews_df \
+            .withColumn('review', lower("review")) \
+            .withColumn('review', regexp_replace("review", "[^a-zA-Z0-9\\s]", "")) \
+            .withColumn('review', split("review", "\\s+")) \
+            .withColumn('review', remove_stop_words_udf('review')) \
+            .withColumn('review', stem_udf('review'))
 
     def splitData(self, training_fraction = 0.7, testing_fraction = 0.3):
         self.training_data, self.testing_data = self.movie_reviews_df.randomSplit([training_fraction, testing_fraction])
