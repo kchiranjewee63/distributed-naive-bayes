9e403a1c4d29eeefdaefd731a8967495d61b56d3
Chiranjeewee Koirala <kchiranjewee63@gmail.com>
2023-04-28 20:49:31 -0400

    Convert all reviews to lower case as part of data preprocessing

--- IMDBMovieReviewData.py
+++ IMDBMovieReviewData.py
@@ -1,3 +1,4 @@
+from pyspark.sql.functions import lower
 class IMDBMovieReviewData:
     def __init__(self, spark_session, data_path):
         self.spark_session = spark_session
@@ -7,12 +8,12 @@ class IMDBMovieReviewData:
         self.movie_reviews_df = self.spark_session.read.csv(self.data_path, header=True, inferSchema=True, quote='"', escape='"')
         
     def preprocessData(self):
+        self.movie_reviews_df = self.movie_reviews_df.withColumn("review", lower(self.movie_reviews_df.review))
 #         ToDO
 #         1. Convert words to lowercase 
 #         2. Remove special characters 
 #         3. Remove stop words 
 #         3. Stemming
-        pass
         
     def splitData(self, training_fraction = 0.7, testing_fraction = 0.3):
         self.training_data, self.testing_data = self.movie_reviews_df.randomSplit([training_fraction, testing_fraction])
